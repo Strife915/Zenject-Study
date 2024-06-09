0168c590d4b2f6e56c298d63f12ee9d7e5ac3e91
Ugur Karabulut <thekarabulut95@gmail.com>
2024-06-09 18:48:53 +0300

    refact: Optimization

--- Zenject Study/Assets/Scripts/FactoryPoolTest/MyTestObjectSpawner.cs
+++ Zenject Study/Assets/Scripts/FactoryPoolTest/MyTestObjectSpawner.cs
@@ -1,41 +1,32 @@
-﻿using System.Threading;
-using Cysharp.Threading.Tasks;
+﻿using Cysharp.Threading.Tasks;
 using Zenject;
 
 public class MyTestObjectSpawner : IInitializable
 {
     MyTestObjectPool _pool;
     MoverFactory _moverFactory;
-    CancellationTokenSource _cancellationTokenSource;
 
     public MyTestObjectSpawner(MyTestObjectPool pool, MoverFactory moverFactory)
     {
         _pool = pool;
         _moverFactory = moverFactory;
-        _cancellationTokenSource = new CancellationTokenSource();
     }
 
     public void Initialize()
     {
-        SpawnProcess(_cancellationTokenSource.Token).Forget();
+        SpawnProcess().Forget();
     }
 
-    async UniTaskVoid SpawnProcess(CancellationToken token)
+    async UniTaskVoid SpawnProcess()
     {
-        // Başlangıçta gecikme görevini oluştur
-        var delayTask = UniTask.Delay(1000, cancellationToken: token);
-
-        while (!token.IsCancellationRequested)
+        while (true)
         {
             var myTestObject = _pool.Spawn();
             var mover = _moverFactory.Create(myTestObject.transform);
             myTestObject.Construct(mover);
             mover.Move();
 
-            await delayTask; // Önceden oluşturulmuş gecikme görevini kullan
-
-            // Gecikme görevini yeniden başlat
-            delayTask = UniTask.Delay(1000, cancellationToken: token);
+            await UniTask.Delay(1000);
         }
     }
 }
