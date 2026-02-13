49a31d2c9f6fa4490df18af0eb9f52b6fd1e16bb
Strife915 <thekarabulut95@gmail.com>
2026-02-13 14:51:42 +0300

    Inject both presenter attackers as IAttacker via ID binding (#3)

--- Zenject Study/Assets/Scripts/Decorator2/Decorator2AttackPresenter.cs
+++ Zenject Study/Assets/Scripts/Decorator2/Decorator2AttackPresenter.cs
@@ -5,16 +5,21 @@ namespace Decorator2
 {
     public class Decorator2AttackPresenter : IInitializable
     {
-        readonly IAttacker _attacker;
+        readonly IAttacker _defaultAttacker;
+        readonly IAttacker _eliteAttacker;
 
-        public Decorator2AttackPresenter(IAttacker attacker)
+        public Decorator2AttackPresenter(
+            IAttacker defaultAttacker,
+            [Inject(Id = Decorator2Installer.EliteAttackerId)] IAttacker eliteAttacker)
         {
-            _attacker = attacker;
+            _defaultAttacker = defaultAttacker;
+            _eliteAttacker = eliteAttacker;
         }
 
         public void Initialize()
         {
-            Debug.Log($"Decorator2 Total Attack Damage: {_attacker.GetAttackDamage()}");
+            Debug.Log($"Decorator2 Default Attacker Damage: {_defaultAttacker.GetAttackDamage()}");
+            Debug.Log($"Decorator2 Elite Attacker Damage: {_eliteAttacker.GetAttackDamage()}");
         }
     }
 }
--- Zenject Study/Assets/Scripts/Decorator2/Decorator2Installer.cs
+++ Zenject Study/Assets/Scripts/Decorator2/Decorator2Installer.cs
@@ -9,6 +9,12 @@ namespace Decorator2
         public const string WeaponBonusId = "Decorator2.WeaponBonus";
         public const string EnchantPercentId = "Decorator2.EnchantPercent";
 
+        public const string EliteAttackerId = "Decorator2.EliteAttacker";
+
+        public const string EliteBaseDamageId = "Decorator2.EliteBaseDamage";
+        public const string EliteWeaponBonusId = "Decorator2.EliteWeaponBonus";
+        public const string EliteEnchantPercentId = "Decorator2.EliteEnchantPercent";
+
         [SerializeField]
         int _baseDamage = 20;
 
@@ -18,16 +24,31 @@ namespace Decorator2
         [SerializeField]
         int _weaponEnchantPercent = 30;
 
+        [Header("Elite Attacker")]
+        [SerializeField]
+        int _eliteBaseDamage = 35;
+
+        [SerializeField]
+        int _eliteWeaponBonusDamage = 25;
+
+        [SerializeField]
+        int _eliteWeaponEnchantPercent = 50;
+
         public override void InstallBindings()
         {
             Container.BindInstance(_baseDamage).WithId(BaseDamageId);
             Container.BindInstance(_weaponBonusDamage).WithId(WeaponBonusId);
             Container.BindInstance(_weaponEnchantPercent).WithId(EnchantPercentId);
 
+            Container.BindInstance(_eliteBaseDamage).WithId(EliteBaseDamageId);
+            Container.BindInstance(_eliteWeaponBonusDamage).WithId(EliteWeaponBonusId);
+            Container.BindInstance(_eliteWeaponEnchantPercent).WithId(EliteEnchantPercentId);
+
             Container.Bind<IAttacker>().To<BaseAttacker>().AsTransient();
             Container.Decorate<IAttacker>().With<WeaponAttackerDecorator>();
             Container.Decorate<IAttacker>().With<WeaponEnchantAttackerDecorator>();
 
+            Container.Bind<IAttacker>().WithId(EliteAttackerId).To<EliteAttacker>().AsSingle();
             Container.BindInterfacesTo<Decorator2AttackPresenter>().AsSingle();
         }
     }
